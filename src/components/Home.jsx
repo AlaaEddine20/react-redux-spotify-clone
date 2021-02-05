84b9bca847627781f451cfc8566b9fa37fa2138d
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-02-05 09:38:29 +0000

    created homepage branch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Row, Image, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 class Home extends React.Component {
   state = {
