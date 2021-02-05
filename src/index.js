3ebbe3e2c7f5d7b7b0f515ebbde2ca98e944c1cf
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-02-05 09:31:14 +0000

    created redux store and dummy reducers

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import configureStore from "./store";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={configureStore()}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
